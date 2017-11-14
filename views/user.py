58f9137ea1d32a05f83757b6a3e70b1eacd22e4b
Guangyangpku <guangyang811@gmail.com>
2017-11-13 23:41:18 -0500

    fix bugs

--- views/user.py
+++ views/user.py
@@ -5,12 +5,14 @@ from flask_login import login_user, login_required, current_user
 
 user_bp = Blueprint('users', __name__)
 
-@user_bp.route('/<lname>', methods=['GET', 'POST'])
+@user_bp.route('/<lname>', methods=['GET', 'POST', 'DELETE'])
 def personList(lname):
     if request.method == 'GET':
         _, uid = current_user.id.split(" ")
-        query = "SELECT R.* FROM Restaurants AS R, Contain AS C WHERE C.did='" + uid + "' AND R.restid=C.restid"
-        cursor = g.conn.execute(query)
+        # query = "SELECT R.* FROM Restaurants AS R, Contain AS C WHERE C.did='" + uid + "', C.lname='" + lname + "' AND R.restid=C.restid"
+        cursor = g.conn.execute('''
+            SELECT R.* FROM Restaurants AS R, Contain AS C WHERE C.did=%s AND C.lname=%s AND R.restid=C.restid
+        ''',(uid, lname))
         paras = ["restid", "name", "street_name", "city", "state", "postal_code", "stars"]
         data = []
         for result in cursor:
@@ -19,6 +21,10 @@ def personList(lname):
         	})
         cursor.close()
         return render_template("perlist.html", data=data)
+
+    if request.method == 'DELETE':
+        pass
+
 
 @user_bp.route('/addList', methods=['POST'])
 @login_required
